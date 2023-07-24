7c9915aad8f916d36ea399abfb592a63c9511ff1
nfottz <121929363+nfottz@users.noreply.github.com>
2023-07-24 17:20:42 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        
+        while(slice * answer < n){
+            answer ++;
+        }
+        return answer;
+    }
+}
