377e84a5e88df5a8e7521cf9925636c53c375147
nfottz <121929363+nfottz@users.noreply.github.com>
2023-07-27 15:50:42 +0900

    [level 1] Title: K번째수, Time: 0.33 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int length = commands.length;
+        int[] answer =  new int[length];
+        for(int i = 0; i < length; i++){
+            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
+            for(int j = 0; j < arr.length; j++){
+                arr[j] = array[j + commands[i][0] - 1];
+            }
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
