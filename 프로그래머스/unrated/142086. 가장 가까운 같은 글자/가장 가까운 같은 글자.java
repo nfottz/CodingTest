3beba8537ff6cb0f6b6054b48a4dfee13111fe3c
nfottz <121929363+nfottz@users.noreply.github.com>
2023-07-31 16:52:14 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 159.90 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -3,16 +3,15 @@ class Solution {
         int[] answer = new int[s.length()];
         answer[0] = -1;
         for(int i = 1; i < s.length(); i++){
-            int temp = -1;
+            answer[i] = -1;
             for(int j = 0; j < i; j++){
                 if(i == j) continue;
                 else {
                     if(s.charAt(j) == s.charAt(i)){
-                        temp = (i - j);
+                        answer[i] = (i - j);
                     }
                 }
             }
-            answer[i] = temp;
         }
         return answer;
     }
