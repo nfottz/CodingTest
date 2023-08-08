e2760a98f78a00cf9a8f652ad242566c63dbdc31
nfottz <121929363+nfottz@users.noreply.github.com>
2023-08-08 17:33:50 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -3,7 +3,6 @@
 class Solution {
     public int solution(int a, int b) {
         
-        // 최대공약수 구하기
         int gcd = 0;
         if(a > b){
             for(int n = 1; n <= b; n++){
@@ -14,9 +13,8 @@ class Solution {
                 if(a % n == 0 && b % n == 0) gcd = n;
             }
         }
-        
-        // 기약분수의 분모 구하기
-        double denom = b / gcd;
+
+        int denom = b / gcd;
 
         while(denom % 2 == 0){
             denom /= 2;
@@ -26,34 +24,7 @@ class Solution {
         }
         
         int answer = 1;
-        System.out.println(denom);
-        for(int i = 3; i <= denom; i++){
-            if(denom % i == 0) {
-                answer = 2; break;
-            }
-        }
-        
-//         // 1000 이하의 소수 구하기
-//         List<Integer> prime = new ArrayList<>();
-//         for(int x = 2; x <= 1000; x++){
-//             int count = 0;
-//             for(int y = 1; y <= 1000; y++){
-//                 if(x % y == 0) count++;
-//             }
-//             if(count == 2) prime.add(x);
-//         }
-        
-//         int answer = 0;
-//         for(int i = 0; i < prime.size(); i++){
-//             if(i == 0 || i == 2){
-//                 continue;
-//             } else {
-//                 if(denom % prime.get(i) == 0){
-//                     answer = 2; return answer;
-//                 }
-//             } answer = 1;
-//         }
-//         if(denom == 1) answer = 1;
+        if(denom != 1) answer = 2;
         
         return answer;
     }
