fa5bdbcdb0c3af816e3689a95e75fa6fe703518d
nfottz <121929363+nfottz@users.noreply.github.com>
2023-08-08 17:30:28 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.54 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 
 class Solution {
     public int solution(int a, int b) {
@@ -18,27 +18,42 @@ class Solution {
         // 기약분수의 분모 구하기
         double denom = b / gcd;
 
-        // 1000 이하의 소수 구하기
-        List<Integer> prime = new ArrayList<>();
-        for(int x = 2; x <= 1000; x++){
-            int count = 0;
-            for(int y = 1; y <= 1000; y++){
-                if(x % y == 0) count++;
-            }
-            if(count == 2) prime.add(x);
+        while(denom % 2 == 0){
+            denom /= 2;
+        }
+        while(denom % 5 == 0){
+            denom /= 5;
         }
         
-        int answer = 0;
-        for(int i = 0; i < prime.size(); i++){
-            if(i == 0 || i == 2){
-                continue;
-            } else {
-                if(denom % prime.get(i) == 0){
-                    answer = 2; return answer;
-                }
-            } answer = 1;
+        int answer = 1;
+        System.out.println(denom);
+        for(int i = 3; i <= denom; i++){
+            if(denom % i == 0) {
+                answer = 2; break;
+            }
         }
-        if(denom == 1) answer = 1;
+        
+//         // 1000 이하의 소수 구하기
+//         List<Integer> prime = new ArrayList<>();
+//         for(int x = 2; x <= 1000; x++){
+//             int count = 0;
+//             for(int y = 1; y <= 1000; y++){
+//                 if(x % y == 0) count++;
+//             }
+//             if(count == 2) prime.add(x);
+//         }
+        
+//         int answer = 0;
+//         for(int i = 0; i < prime.size(); i++){
+//             if(i == 0 || i == 2){
+//                 continue;
+//             } else {
+//                 if(denom % prime.get(i) == 0){
+//                     answer = 2; return answer;
+//                 }
+//             } answer = 1;
+//         }
+//         if(denom == 1) answer = 1;
         
         return answer;
     }
