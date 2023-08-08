df349452f33d92b2027e6a98aad7065e5a4fdcf7
nfottz <121929363+nfottz@users.noreply.github.com>
2023-08-08 19:57:31 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -7,8 +7,6 @@ class Solution {
             answer = 1;
         } else if(((double)dots[0][1] - dots[3][1])/((double)dots[0][0] - dots[3][0]) == ((double)dots[2][1] - dots[1][1])/((double)dots[2][0] - dots[1][0])) {
             answer = 1;
-        } else {
-            answer = 0;
         }
         return answer;
     }
