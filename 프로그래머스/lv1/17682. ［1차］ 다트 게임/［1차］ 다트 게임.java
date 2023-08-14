fe9df5c39de737fd5a95f7d17657620c9b073b44
nfottz <121929363+nfottz@users.noreply.github.com>
2023-08-14 18:04:35 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.20 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dartResult) {
+
+        String[] bonus = new String[3];
+        int j = 0;
+        for(int i = 0, length = dartResult.length(); i < length; i++){
+            String s = dartResult.substring(i, i + 1);
+            if(s.equals("S") || s.equals("D") || s.equals("T")){
+                bonus[j] = s;
+                dartResult = dartResult.replaceFirst(s, " ");
+                j++;
+            }
+        }
+        String[] temp = dartResult.split(" ");
+        String[] option = new String[3];
+        for(int i = 1, length = temp.length; i < length; i++){
+            if(temp[i].contains("*")) {
+                option[i - 1] = "*";
+                temp[i] = temp[i].replace("*", "");
+            } else if(temp[i].contains("#")) {
+                option[i - 1] = "#";
+                temp[i] = temp[i].replace("#", "");
+            }
+        }
+        int[] scores = new int[3];
+        for(int i = 0; i < 3; i++){
+            scores[i] = Integer.parseInt(temp[i]);
+            switch(bonus[i]){
+                case "D" : scores[i] = scores[i] * scores[i]; break;
+                case "T" : scores[i] = scores[i] * scores[i] * scores[i]; break;
+            }
+            if(i == 0){
+                if(option[i] != null){
+                    if(option[i].equals("*")) scores[i] *= 2;
+                    if(option[i].equals("#")) scores[i] *= -1;
+                }
+            } else {
+                if(option[i] != null){
+                    if(option[i].equals("*")) {
+                        scores[i] *= 2;
+                        scores[i - 1] *= 2;
+                    } if(option[i].equals("#")) {
+                        scores[i] *= -1;
+                    }
+                }
+            }
+        }
+        int answer = 0;
+        for(int i = 0; i < 3; i++){
+            answer += scores[i];
+        }
+        return answer;
+    }
+}
