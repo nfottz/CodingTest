db161dec9655c6390c52892087a7819500f1d8dd
nfottz <121929363+nfottz@users.noreply.github.com>
2023-08-16 13:47:43 +0900

    [unrated] Title: 대충 만든 자판, Time: 10.86 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        int[] key = new int[26];
+        for(int i = 0; i < 26; i++){
+            int min = 100;
+            for(int j = 0, length = keymap.length; j < length; j++){
+                char ch = (char)(i + 65);
+                int k = keymap[j].indexOf(ch);
+                if(k != -1) min = Math.min(min, k);
+            }
+            key[i] = min + 1;
+        }
+        System.out.println(Arrays.toString(key));
+        for(int i = 0, length = targets.length; i < length; i++){
+            for(int j = 0, size = targets[i].length(); j < size; j++){
+                int temp = (int)targets[i].charAt(j) - 65;
+                System.out.println(temp);
+                if(key[temp] == 101){
+                    answer[i] = -1; break;
+                } else {
+                    answer[i] += key[temp];
+                }
+            }
+        }
+        return answer;
+    }
+}
