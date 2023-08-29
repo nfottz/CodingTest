777de6ec74eac048997a70f8127203555bd9e864
nfottz <121929363+nfottz@users.noreply.github.com>
2023-08-29 20:17:44 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for(int i = 1; i <= num; i++){
+            answer += i;
+        }
+        System.out.println(answer);
+    }
+}
